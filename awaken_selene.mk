e64cc4de17c576e94ea24d9aeaf707f7bcbbb046
YudhoPatrianto <kydh01123@gmail.com>
2023-09-16 20:59:01 +0700

    selene: awaken-ivy
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- awaken_selene.mk
+++ awaken_selene.mk
@@ -7,9 +7,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Arrow stuff
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common ProjectAwaken stuff
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 #
 # All components inherited here go to system_ext image
@@ -28,16 +29,18 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
 
-#CherishOS Flags
-CHERISH_BUILD_TYPE := UNOFFICIAL
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=YudhoPatrianto
+# Project Awaken Stuff
+AWAKEN_BUILD_TYPE := UNOFFICIAL 
+USE_GAPPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_BUILD_APERTURE_CAMERA := true
+WITH_GAPPS := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
-PRODUCT_NAME := cherish_selene
+PRODUCT_NAME := awaken_selene
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 10
@@ -53,4 +56,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Redmi/selene/selene:13/TP1A.220624.014/V14.0.3.0.TKUEUXM:user/release-keys"
+BUILD_FINGERPRINT := Redmi/selene/selene:13/TP1A.220624.014/V14.0.3.0.TKUEUXM:user/release-keys
