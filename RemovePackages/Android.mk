22bb84bdde93f0d37a9f9582b6ac0472d581c60a
YudhoPatrianto <kydh01123@gmail.com>
2023-09-18 20:19:09 +0700

    selene: RemovedPackages: updated removed list
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,10 +5,51 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    arcore \
+    Chrome \
+    Chrome-Stub \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
     Drive \
+    GCS \
+    GoogleCamera \
+    GoogleCameraGo \
+    GrapheneOS-Camera \
     Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PixelLiveWallpaperPrebuilt \
+    Photos \
+    PlayAutoInstallConfig \
     PrebuiltGmail \
-    YouTube
+    RecorderPrebuilt \
+    SafetyHubPrebuilt \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    Tycho \
+    USCCDM \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
