d9afc706f0756e480d6f5dfbf9b5a1f12c523884
YudhoPatrianto <kydh01123@gmail.com>
2023-10-10 11:58:40 +0700

    selene: Build With Lineage Health
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- device.mk
+++ device.mk
@@ -278,6 +278,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     PowerOffAlarm
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
