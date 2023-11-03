1489b399c2fd6192300668d3cd2023c49858a837
YudhoPatrianto <kydh01123@gmail.com>
2023-11-04 04:49:25 +0700

    selene: bringup SparkOS For selene
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- spark_selene.mk
+++ spark_selene.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Aosp stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common SparkOS stuff
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 #
 # All components inherited here go to system_ext image
@@ -30,9 +30,24 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
+# SparkOS Maintainer Flags
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.spark.device.name=selene \
+    ro.spark.group.url=https://linktr.ee/YudhoPatrianto \
+    ro.spark.maintainer=YudhoPatrianto \
+    ro.spark.maintainer=YudhoPatrianto
+
+# GoogleApps Flags
+WITH_GAPPS := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
-PRODUCT_NAME := aosp_selene
+PRODUCT_NAME := spark_selene
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 10
